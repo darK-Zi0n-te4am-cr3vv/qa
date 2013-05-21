be2dd1d3dcca23318f432cd2f7eb89b80e9b7b79
unknown <ccik@inbox.ru>
2013-05-22 01:36:36 +0400

    Added new supported course http://www.intuitu.net/category/Kategorii-slovarej/Kriptograficheskie-osnovy-bezopasnosti

--- intuitu.net/parse.bat
+++ intuitu.net/parse.bat
@@ -3,3 +3,4 @@
 mkdir parsed
 
 .\bin\parser http://www.intuitu.net/category/Kategorii-slovarei/Protokoly-bezopasnogo-setevogo-vzaimodeistviYa > .\parsed\Protokoly-bezopasnogo-setevogo-vzaimodeistviYa.md
+.\bin\parser http://www.intuitu.net/category/Kategorii-slovarej/Kriptograficheskie-osnovy-bezopasnosti > .\parsed\Kriptograficheskie-osnovy-bezopasnosti.md
