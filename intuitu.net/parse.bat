d9ce1d4c9a77193e909882da821e46614d0c2175
unknown <ccik@inbox.ru>
2013-05-22 01:31:54 +0400

    Added parse.bat -- file for batch parsing

---
+++ intuitu.net/parse.bat
@@ -0,0 +1,5 @@
+@echo off
+
+mkdir parsed
+
+.\bin\parser http://www.intuitu.net/category/Kategorii-slovarei/Protokoly-bezopasnogo-setevogo-vzaimodeistviYa > .\parsed\Protokoly-bezopasnogo-setevogo-vzaimodeistviYa.md
