cb1a8bd5ef19f169d1d0a902d3b0287da133f0d7
unknown <ccik@inbox.ru>
2013-05-22 00:08:12 +0400

    Added obtaining of last page index

--- intuitu.net/parser/Program.cs
+++ intuitu.net/parser/Program.cs
@@ -1,4 +1,7 @@
-﻿using HtmlAgilityPack;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
 
 namespace QA.Inituitu.Net.Parser
 {
@@ -10,8 +13,26 @@ namespace QA.Inituitu.Net.Parser
             return web.Load(url);
         }
 
+        private static int GetLastPageIndex(HtmlDocument doc)
+        {
+            var lastPageLink = doc.DocumentNode
+                                  .SelectNodes("//li[@class='pager-last last']/a")
+                                  .Single()
+                                  .GetAttributeValue("href", null);
+
+            return Int32.Parse(Regex.Match(lastPageLink, @"\?page=(\d+)").Groups[1].Value);
+        }
+
         static void Main(string[] args)
         {
+            const string baseUrl =
+                "http://www.intuitu.net/category/Kategorii-slovarei/Protokoly-bezopasnogo-setevogo-vzaimodeistviYa";
+
+            var baseDoc = DownloadDocument(baseUrl);
+
+            var lastPageIndex = GetLastPageIndex(baseDoc);
+
+            Console.WriteLine(lastPageIndex);
         }
     }
 }
