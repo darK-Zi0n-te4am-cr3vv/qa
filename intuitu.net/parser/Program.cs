9db83afa19f1c395de68606797f28b3be3249848
unknown <ccik@inbox.ru>
2013-05-21 23:55:27 +0400

    Added method Program::DownloadDocument()

--- intuitu.net/parser/Program.cs
+++ intuitu.net/parser/Program.cs
@@ -1,7 +1,15 @@
-﻿namespace QA.Inituitu.Net.Parser
+﻿using HtmlAgilityPack;
+
+namespace QA.Inituitu.Net.Parser
 {
     class Program
     {
+        private static HtmlDocument DownloadDocument(string url)
+        {
+            var web = new HtmlWeb();
+            return web.Load(url);
+        }
+
         static void Main(string[] args)
         {
         }
