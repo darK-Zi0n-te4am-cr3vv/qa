5966d8595b08ba5692028907a56db0b5ecdce621
unknown <ccik@inbox.ru>
2013-05-21 23:41:18 +0400

    Changed namespace in Program.cs

--- intuitu.net/parser/Program.cs
+++ intuitu.net/parser/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace parser
+﻿namespace QA.Inituitu.Net.Parser
 {
     class Program
     {
